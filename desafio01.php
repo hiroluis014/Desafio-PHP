5d3663ac6e403109f5d76c42b424f8ab505ab142
Hiro Luis <hiroluis014@gmail.com>
2026-06-16 11:59:47 -0300

    Desafio-01.php

--- desafio01.php
+++ desafio01.php
@@ -1,8 +1,7 @@
 <?php
 function localizarNumero($numero)
 {
-    $numeros = range(1, 31);
-    shuffle($numeros);
+    $numeros = [10, 7, 15, 16, 21, 4, 11, 31, 25, 22, 29, 9, 26, 6, 14, 30, 8, 12, 24, 17, 5, 23, 3, 18, 13, 27, 2, 28, 1, 20, 19];
     echo "Array gerado: ";
     print_r($numeros);
     echo "<br><br>";
@@ -15,10 +14,9 @@ function localizarNumero($numero)
 }
 
 $numeroProcurado = 29;
-
 echo localizarNumero($numeroProcurado);
-
 ?>
+
 
 
  
